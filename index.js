0ced15ec986fa726e94a2b4db3c1b04a17776eb1
Your Name <you@example.com>
2019-02-17 14:56:51 +0900

    2019.02.17

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // index.js
-const f = fn=>{
+const cache = f=>{
     let initialized = false
     let cache
     return (...args)=>{
         if(!initialized){
             initialized = true
-            cache = fn()
+            cache = f()
 	}
         return cache.apply(null, args)
     }
 }
-module.exports = f
+module.exports = cache
+
+if(module.parent) return
+
+// sample
+const f = cache(()=>{
+    const dt = Date.now()
+    console.log(`${dt}: called.`)
+    return v=>`${Date.now()}: v=[${v}], dt=[${dt}].`
+})
+
+console.log(f('foo'))
+console.log(f('bar'))
